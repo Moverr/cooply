7aee530946ba02483f05529a8e81741bc278265c
Muyinda Rogers <muyinda.rodgers@wasoko.com>
2025-02-21 22:16:25 +0300

    Building Splash Screen

---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class SplashScreen extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() => SplashScreenState();
+
+
+}
+
+class SplashScreenState extends State<SplashScreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
