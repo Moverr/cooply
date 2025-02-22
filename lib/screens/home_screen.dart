e79f1811b9209dd6d521ba2eca7d081e6a74d9b2
Muyinda Rogers <muyinda.rodgers@wasoko.com>
2025-02-23 00:31:43 +0300

    Adding Tabs to the Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,78 +1,91 @@
+import 'dart:ui';
+
+import 'package:cooply/screens/login_logout_screen.dart';
 import 'package:cooply/widgets/SimpleTextWidget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 // @movers
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-
 
     double offsetScreen = screenHeight * 0.12;
 
     return Scaffold(
-    body: ListView(
+      appBar: PreferredSize(preferredSize: Size.fromHeight(200),
+          child:   AppBar(
+            // title: Text("Mawae"),
+            flexibleSpace: Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  color: Color(0xFFEAF5E4),
+                  child: ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(0.5), // Adjust opacity (0.0 to 1.0)
+                      BlendMode.dstATop, // Blend the opacity with the image
+                    ),
+                    child: Image.asset('assets/home_image.png', fit: BoxFit.cover),
+                  ),
+                ),
+                Container(
 
-      shrinkWrap: true,  // Prevents unnecessary extra space
-      physics: NeverScrollableScrollPhysics(), // Disables independent scrolling
-      padding: EdgeInsets.zero, // Removes default padding
-
-      children: [
-
-Stack(
-  alignment: Alignment.center,
-  children: [
-
-    Container(
-width: double.infinity,
-      height: 200,
-        // color: Color(0xFFEAF5E4),
-      child: Image.asset('assets/home_image.png',
-        fit: BoxFit.cover
-      ),
-    ),
-
-    Container(
-
-      width: double.infinity,
-      height: 200,
-      child:  SimpleTextWidget(
-        title: " \n \n  \n \n \n \n khoodilabs © 2025 ",
-        fontFamily: "Inria Serif",
-        fontWeight: FontWeight.normal,
-        fontSize: 10,
-      ),
-      // color: Colors.red,
-    ),
+                  child: Center(child: Image.asset(
+                    'assets/cooply_sm.png',
+                    // Adjust how the image fits inside the box
+                  ),),
+                ),
 
 
-  ],
-),
-
-        Container(
-          // padding: EdgeInsets.only(left: 10, top: 65, right: 20, bottom: 15),
-          height: screenHeight * 0.30, color: Color(0xFFEAF5E4),
-          child: Image.asset('assets/home_screen_image.png',
-           fit: BoxFit.fill,
-          ),
-          //assets/home_screen_image.png
-        ),
-
-       Transform.translate(
-         offset:   Offset(0, 0),
-         child: Container(
-           height: screenHeight * 0.75, color: Colors.deepOrange.shade100,
-           child: Text("LOW  Layer"),
-         ),
-       ),
 
 
-      ],
-    )
-  );
+              ],
+            )
+
+                /*
+            Container(
+              height: 400,
+              child: Center( child: Text("SEEEE")),
+              color: Colors.yellow.shade100,
+
+            ),*/
+          )
+      )
+
+      ,
+        body:
+     DefaultTabController(
+            length: 2,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(0)),
+                  ),
+                  child: TabBar(
+                    labelColor: Colors.white,
+                    unselectedLabelColor: Colors.white70,
+                    indicatorColor: Colors.white,
+                    tabs: [
+                      Tab(text: "LOGIN"),
+                      Tab(text: "SIGN UP"),
+                    ],
+                  ),
+                ),
+                Expanded(child: TabBarView(children: [Text("teee"), Text("veeee")]))
+              ],
+            ),
+          )
+
+
+
+    );
   }
-  
 }
