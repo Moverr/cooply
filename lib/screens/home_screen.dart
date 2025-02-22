aaa961197737f096eb9791c47079e450c5c61e8a
Muyinda Rogers <muyinda.rodgers@wasoko.com>
2025-02-22 17:34:32 +0300

    Home Page, Adding cooply sm

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,77 @@
+import 'package:cooply/widgets/SimpleTextWidget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// @movers
 class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    body: Center(
-      child: Text("HOme Screen"),
+
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+
+    double offsetScreen = screenHeight * 0.12;
+
+    return Scaffold(
+    body: ListView(
+
+      shrinkWrap: true,  // Prevents unnecessary extra space
+      physics: NeverScrollableScrollPhysics(), // Disables independent scrolling
+      padding: EdgeInsets.zero, // Removes default padding
+
+      children: [
+
+Stack(
+  alignment: Alignment.center,
+  children: [
+
+    Container(
+width: double.infinity,
+      height: 200,
+        // color: Color(0xFFEAF5E4),
+      child: Image.asset('assets/home_image.png',
+        fit: BoxFit.cover
+      ),
     ),
+
+    Container(
+
+      width: double.infinity,
+      height: 200,
+      child:  SimpleTextWidget(
+        title: " \n \n  \n \n \n \n khoodilabs © 2025 ",
+        fontFamily: "Inria Serif",
+        fontWeight: FontWeight.normal,
+        fontSize: 10,
+      ),
+      // color: Colors.red,
+    ),
+
+
+  ],
+),
+
+        Container(
+          // padding: EdgeInsets.only(left: 10, top: 65, right: 20, bottom: 15),
+          height: screenHeight * 0.30, color: Color(0xFFEAF5E4),
+          child: Image.asset('assets/home_screen_image.png',
+           fit: BoxFit.fill,
+          ),
+          //assets/home_screen_image.png
+        ),
+
+       Transform.translate(
+         offset:   Offset(0, 0),
+         child: Container(
+           height: screenHeight * 0.75, color: Colors.deepOrange.shade100,
+           child: Text("LOW  Layer"),
+         ),
+       ),
+
+
+      ],
+    )
   );
   }
   
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -9,37 +9,28 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
-
- 
   @override
   Widget build(BuildContext context) {
-
-    Future.delayed( Duration(seconds: 5),(){
-
-      //todo: open the home screen 
-      Navigator.pushReplacement(context, 
-      MaterialPageRoute(builder: (context)=> HomeScreen())
-      );
-    }
-
-    );
-
-
+    Future.delayed(Duration(seconds: 5), () {
+      //todo: open the home screen
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomeScreen()));
+    });
 
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-            child: SplashImageWidget(),
-            height: screenHeight * 0.86,
-          ),
-          Container(
-
-            height: screenHeight * 0.10,color: Color(0xFFE1EAD1),
-            child:  Column(
+        body: ListView(
+      children: [
+        Container(
+          child: SplashImageWidget(),
+          height: screenHeight * 0.86,
+        ),
+        Container(
+            height: screenHeight * 0.10,
+            color: Color(0xFFE1EAD1),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
@@ -48,34 +39,19 @@ class SplashScreen extends StatelessWidget {
                   fontFamily: "Inria Serif",
                   fontWeight: FontWeight.normal,
                   fontSize: 25,
-                    ),
-
+                ),
                 SimpleTextWidget(
                   title: "khoodilabs © 2025 ",
                   fontFamily: "Inria Serif",
                   fontWeight: FontWeight.normal,
                   fontSize: 10,
                 )
-
-
-
               ],
             )
             //Text("Footer",textAlign: TextAlign.center,),
 
-          )
-
-        ],
-
-      )
-
-
-
-      
-    );
+            )
+      ],
+    ));
   }
 }
-
-
-
-
