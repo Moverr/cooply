cf7f0787f058f2cc81ddfcce5c29eb9b70a2e259
Muyinda Rogers <muyinda.rodgers@wasoko.com>
2025-06-10 18:12:35 +0300

    Schedule Screen

--- lib/screens/dashboard/MainScreens/schedule_screen.dart
+++ lib/screens/dashboard/MainScreens/schedule_screen.dart
@@ -1,7 +1,18 @@
 
+import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../models/dtos/Farm.dart';
+import '../../../models/dtos/feed_inventory.dart';
+import '../../../models/dtos/loginResponse.dart';
+import '../../../services/FarmService.dart';
+import '../../../utils/AppConstants.dart';
+import '../../../widgets/feedListTyle.dart';
+
+
 
 class ScheduleScreen extends StatefulWidget{
   @override
@@ -10,14 +21,423 @@ class ScheduleScreen extends StatefulWidget{
 }
 
 class _ScheduleState extends State<ScheduleScreen>{
+
+  TextEditingController _searchController = TextEditingController();
+
+  List<Map<String, String>> _filteredData = [];
+
+  late FarmDataSource _farmDataSource;
+  final FarmService _farmService = FarmService();
+
+  @override
+  void initState() {
+    super.initState();
+    _farmDataSource = FarmDataSource(context);
+    _farmDataSource.fetchPage(0);
+  }
+
+  FarmDataSource Function(BuildContext, LoginResponse) initFarmDataSource =
+      (context, loginResponse) => FarmDataSource(context);
+
+  // Separate async method for initialization
+  Future<void> _initializeData() async {
+    await loadUser(); // Wait for loginResponse to be ready
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  // Filter data based on search query
+  void _filterData() {
+    /*  final query = _searchController.text.toLowerCase();
+    setState(() {
+      _filteredData = _dataSource.getData().where((row) {
+        return row['name']!.toLowerCase().contains(query) ||
+            row['account']!.toLowerCase().contains(query) ||
+            row['status']!.toLowerCase().contains(query);
+      }).toList();
+    });
+
+    */
+  }
+
+  late LoginResponse? loginResponse;
+  Future<LoginResponse?> loadUser() async {
+    final user = await getLoginResponse();
+    if (user != null) {
+      return user;
+      // _farmDataSource = FarmDataSource(this.context,user);
+    } else {
+      return null;
+    }
+  }
+
+  Future<LoginResponse?> getLoginResponse() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final jsonString = prefs.getString('login_response');
+
+      if (jsonString == null || jsonString.isEmpty) {
+        debugPrint('⚠️ No login_response found.');
+        return null;
+      }
+
+      final Map<String, dynamic> json = jsonDecode(jsonString);
+      final loginResponse = LoginResponse.fromJson(json);
+
+      debugPrint('✅ Loaded LoginResponse: $json');
+      return loginResponse;
+    } catch (e, stack) {
+      debugPrint('❌ Failed to load LoginResponse: $e\n$stack');
+      return null;
+    }
+  }
+
+  bool _isLoading = true;
+
+  final List<FeedInventory> items = [
+    FeedInventory(
+        id: 1,
+        batchId: "1202202401",
+        quantity: 12344,
+        availableQuantity: 12344,
+        transactionType: "ADDITION", //ADDITION,REDUCTION
+        farm: "Mwamba Farm",
+        flock: "BN:1234567", //?? which coop
+
+        author: "Muyinda ROgers",
+        registeredDate: "12-10-2024",
+        createdOn: "12-10-2024",
+        modifiedOn: "12-10-2024",
+        items: [
+          FeedInventoryLog(
+              id: 1,
+              itemName: "Maize",
+              quantity: 1000, //consumed or added
+              availableQuantity: 1100,
+              transactionType: "ADDITION")
+        ]),
+    FeedInventory(
+        id: 1,
+        batchId: "1202202401",
+        quantity: 12344,
+        availableQuantity: 12344,
+        transactionType: "ADDITION", //ADDITION,REDUCTION
+        farm: "Mwamba Farm",
+        flock: "BN:1234567", //?? which coop
+
+        author: "Muyinda ROgers",
+        registeredDate: "12-10-2024",
+        createdOn: "12-10-2024",
+        modifiedOn: "12-10-2024",
+        items: [
+          FeedInventoryLog(
+              id: 1,
+              itemName: "Maize",
+              quantity: 1000, //consumed or added
+              availableQuantity: 1100,
+              transactionType: "ADDITION")
+        ]),
+  ];
+
+  String selectedValue = 'Apple';
+  final List<String> dropDownItems = ['Apple', 'Banana', 'Mango', 'Orange'];
+
+
+
   @override
   Widget build(BuildContext context) {
 
+
     return Scaffold(
 
-      body: Text(" Schedule Screen ")
+      body: Column(
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ExpansionTile(
+            title: Text(
+              "Feeds",
+              style:
+              TextStyle(fontSize: 20, fontFamily: AppConstants.fontFamily),
+            ),
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade400),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      value: selectedValue,
+                      isExpanded: true,
+                      icon: const Icon(Icons.arrow_drop_down),
+                      style: const TextStyle(fontSize: 16, color: Colors.black),
+                      onChanged: (String? value) {
+                        setState(() {
+                          selectedValue = value!;
+                        });
+                      },
+                      hint: const Text(
+                        "Select an option",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      items: dropDownItems
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade400),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      value: selectedValue,
+                      isExpanded: true,
+                      icon: const Icon(Icons.arrow_drop_down),
+                      style: const TextStyle(fontSize: 16, color: Colors.black),
+                      onChanged: (String? value) {
+                        setState(() {
+                          selectedValue = value!;
+                        });
+                      },
+                      hint: const Text(
+                        "Select an option",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      items: dropDownItems
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    labelText: 'Search',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    ),
+                    prefixIcon: Icon(Icons.search),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return FeedListTyle(
+                    feed: items[index],
+                    onTap: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Tapped ${items[index]}')),
+                      );
+                    },
+                  );
+
+                },
+              )),
+
+
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Your action here
+        },
+        icon: Icon(Icons.add),
+        label: Text("Add"),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
 
     );
   }
 
 }
+
+class FarmDataSource extends DataTableSource {
+  final BuildContext context;
+
+  List<Farm> farms = [];
+  int totalRows = 0;
+  int rowsPerPage = 2;
+  int page = 0;
+
+  FarmDataSource(this.context);
+
+  Future<LoginResponse?> getLoginResponse() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final jsonString = prefs.getString('login_response');
+
+      if (jsonString == null || jsonString.isEmpty) {
+        debugPrint('⚠️ No login_response found.');
+        return null;
+      }
+
+      final Map<String, dynamic> json = jsonDecode(jsonString);
+      final loginResponse = LoginResponse.fromJson(json);
+
+      debugPrint('✅ Loaded LoginResponse: $loginResponse');
+      return loginResponse;
+    } catch (e, stack) {
+      debugPrint('❌ Failed to load LoginResponse: $e\n$stack');
+      return null;
+    }
+  }
+
+  Future<void> fetchPage(int pageIndex) async {
+    LoginResponse? loginResponse = await getLoginResponse();
+
+    FarmService farmService = FarmService();
+
+    final offset = pageIndex * rowsPerPage;
+    try {
+      final response = await farmService.fetchFarms(
+          accountId: 16,
+          offset: offset,
+          limit: rowsPerPage,
+          loginResponse: loginResponse);
+
+      farms = response!.content.cast<Farm>();
+      totalRows = response.totalElements;
+      page = response.pageNumber;
+      debugPrint("Reached this Part :-------");
+      notifyListeners();
+    } catch (e) {
+      farms = [];
+      totalRows = 0;
+      notifyListeners();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error fetching farms: $e")),
+      );
+    }
+  }
+
+  final List<Map<String, String>> _data = List.empty();
+
+  @override
+  DataRow? getRow(int index) {
+    if (index >= farms.length) return null;
+    final farm = farms[index];
+    return DataRow.byIndex(index: index, cells: [
+      DataCell(Text(farm.name.toString())),
+      DataCell(Text(farm.status.toString())),
+      DataCell(Text(farm.modifiedOn.toString())),
+      getActionCell(farm.id)
+    ]);
+  }
+
+  DataCell getActionCell(int index) {
+    return DataCell(Row(
+      children: [
+        // Edit Button
+        IconButton(
+          icon: const Icon(Icons.edit, color: Colors.blue),
+          onPressed: () {
+            print('Edit clicked for row $index');
+            // Handle Edit action
+          },
+        ),
+        // Delete Button
+        IconButton(
+          icon: const Icon(Icons.delete, color: Colors.red),
+          onPressed: () {
+            print('Delete clicked for row $index');
+            // Handle Delete action
+          },
+        ),
+        // View Button
+        IconButton(
+          icon: const Icon(Icons.visibility, color: Colors.green),
+          onPressed: () {
+            print('View clicked for row $index');
+            // Handle View action
+          },
+        ),
+      ],
+    ));
+  }
+
+  /*
+  @override
+  DataRow? getRow(int index) {
+    if (index >= _data.length) return null;
+    final row = _data[index];
+    return DataRow(cells: [
+      // DataCell(Text(row['account']!)),
+      DataCell(Text(row['name']!)),
+      DataCell(Text(row['status']!)),
+      // DataCell(Text(row['author']!)),
+      DataCell(Text(row['dateCreated']!)),
+      DataCell(
+        Row(
+          children: [
+            // Edit Button
+            IconButton(
+              icon: const Icon(Icons.edit, color: Colors.blue),
+              onPressed: () {
+                print('Edit clicked for row $index');
+                // Handle Edit action
+              },
+            ),
+            // Delete Button
+            IconButton(
+              icon: const Icon(Icons.delete, color: Colors.red),
+              onPressed: () {
+                print('Delete clicked for row $index');
+                // Handle Delete action
+              },
+            ),
+            // View Button
+            IconButton(
+              icon: const Icon(Icons.visibility, color: Colors.green),
+              onPressed: () {
+                print('View clicked for row $index');
+                // Handle View action
+              },
+            ),
+          ],
+        )
+      ),
+    ]);
+  }
+  */
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => totalRows;
+
+  @override
+  int get selectedRowCount => 0;
+
+  List<Map<String, String>> getData() => _data;
+}
+
